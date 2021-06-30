3f4ce350e2b7c66355eb3f653561f0fd3a750a67
HyejuEs <86722666+HyejuEs@users.noreply.github.com>
2021-06-30 21:09:15 +0900

    Add files via upload

---
+++ StudentInfoCard.java
@@ -0,0 +1,34 @@
+
+/**
+ * Purpose: Display a Student Information to the screen to share with my instructure. 
+ * 
+ * @author Hyeju Cho 
+ * @version September 6, 2020
+ */
+public class StudentInfoCard
+{
+    public static void main(String[ ] args)
+    {
+        System.out.println("***************************************************");
+        System.out.println("*                                                 *");
+        System.out.println("*              Student info                       *");
+        System.out.println("*                                                 *");
+        System.out.println("***************************************************");
+        System.out.println("Name: Esther(Hyeju) Cho");
+        System.out.print("Birthday: March 30, 2002");
+        System.out.println(" Age: 18");
+        System.out.print("Grade: 12th");
+        System.out.println("School: Lanier Christian Academy");
+        System.out.print("City: Flowery Branch, Ga");
+        System.out.println(" Email: hyejucho2018@lanierchristian.org");
+        System.out.print("Availabe: 10a.m. - 11 a.m Mon-Fri");
+        System.out.println(" Guardian: dane.usaiec.gmail.com");
+        System.out.println("Highest math course: AP Calculus AB");
+        System.out.println("Previous computer programing experience: No");
+        System.out.println("People will not be able to live without a computer");
+        System.out.println("in the future. I decided to take this course because"); 
+        System.out.println("I wanted to study more about my computer which will");
+        System.out.println("be closely related with my life in the future."); 
+    }//end of main method
+}//end of class
+
