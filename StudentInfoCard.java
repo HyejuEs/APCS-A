bcaea264f875c2eaac7c70ca31a08deb19fb4408
HyejuEs <86722666+HyejuEs@users.noreply.github.com>
2021-06-30 21:12:08 +0900

    Update StudentInfoCard.java

--- StudentInfoCard.java
+++ StudentInfoCard.java
@@ -14,20 +14,20 @@ public class StudentInfoCard
         System.out.println("*              Student info                       *");
         System.out.println("*                                                 *");
         System.out.println("***************************************************");
-        System.out.println("Name: Esther(Hyeju) Cho");
-        System.out.print("Birthday: March 30, 2002");
+        System.out.println("Name: Esther Cho");
+        System.out.print("Birthday: April 23, 2002");
         System.out.println(" Age: 18");
         System.out.print("Grade: 12th");
-        System.out.println("School: Lanier Christian Academy");
-        System.out.print("City: Flowery Branch, Ga");
-        System.out.println(" Email: hyejucho2018@lanierchristian.org");
+        System.out.println("School: Some Christian Academy");
+        System.out.print("City: City of Star");
+        System.out.println(" Email: 123@gmail.com");
         System.out.print("Availabe: 10a.m. - 11 a.m Mon-Fri");
-        System.out.println(" Guardian: dane.usaiec.gmail.com");
+        System.out.println(" Guardian: sowhat.gmail.com");
         System.out.println("Highest math course: AP Calculus AB");
         System.out.println("Previous computer programing experience: No");
         System.out.println("People will not be able to live without a computer");
-        System.out.println("in the future. I decided to take this course because"); 
-        System.out.println("I wanted to study more about my computer which will");
+        System.out.println("I decided to take this course because"); 
+        System.out.println("I wanted to study more about that computer which will");
         System.out.println("be closely related with my life in the future."); 
     }//end of main method
 }//end of class
